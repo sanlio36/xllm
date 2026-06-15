3ce9a560323aa7c7def29ae4c4f348772f24aaea
shenxiaolong <1193789086@qq.com>
2026-06-15 12:03:32 +0800

    bugfix: fix stop_ids check while ignore eos.

--- xllm/core/framework/request/stopping_checker.cpp
+++ xllm/core/framework/request/stopping_checker.cpp
@@ -74,7 +74,8 @@ FinishReason StoppingChecker::check(const Slice<int32_t>& token_ids,
   }
 
   // check stop tokens
-  if (stop_tokens_.count(last_token_id) > 0) {
+  if ((!ignore_eos_ || last_token_id != eos_token_) &&
+      stop_tokens_.count(last_token_id) > 0) {
     return FinishReason::STOP;
   }
 
